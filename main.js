132d7961c187fe7ebced113238a76723ecf70146
KIARA SMALL <SMALLKAY1726@GMAIL.COM>
2021-09-02 10:10:58 -0500

    commit on main

--- main.js
+++ main.js
@@ -3,3 +3,20 @@
 
 // try some loop stuff
 
+//for
+//while
+//do-while
+
+
+//Question? Which loop do i use and when?
+//for - when I know how many
+//while - just keep doing it until i'm done
+//loop until we reach 10
+for (let idx = 1; idx <= 10; idx++)
+{
+    // we have the code we want repeat
+    console.log(`Current iteration is ${idx}`);
+}
+
+// this line of code will run when we exit the loop
+alert(`Done with the loop thingy`);
