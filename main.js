a6a15a39c4218e3e982ba17761cae4073b10f171
KIARA SMALL <SMALLKAY1726@GMAIL.COM>
2021-09-02 14:34:06 -0500

    commit on main'

--- main.js
+++ main.js
@@ -33,15 +33,41 @@
 //     }
 // }
 
+//One Off timer
 //Write the JavaScript code to display BREAK OFF THE ATTACK! Their shields are still up! 
 //then after 3 seconds (3000 ms) in the console display IT'S A TRAP! using a one off timer.
-console.log('BREAK OFF THE ATTACK! Their shields are still up!')
-setTimeout(itsATrap,5000); // call the function itsatrap after 3 seconds
+// console.log('BREAK OFF THE ATTACK! Their shields are still up!')
+// setTimeout(itsATrap,5000); // call the function itsatrap after 3 seconds
 
-//this is our callback function
-function itsATrap()
+// //this is our callback function
+// function itsATrap()
+// {
+//     const msg = "IT'S A TRAP!";
+//     console.log(msg);
+//     alert(msg);
+// }
+
+//Repeating timer
+setInterval(myTimer, 1000);
+
+let kounter = 0; //init a counter
+
+function myTimer() 
 {
-    const msg = "IT'S A TRAP!";
-    console.log(msg);
-    alert(msg);
+    const d = new Date();
+    document.getElementById("demo").innerHTML = d.toLocaleTimeString();
+    kounter++;
+    //get reference to the variable and check it
+    if (kounter === 11)
+    {
+        
+        clearInterval(myTimer);
+        console.log(`Timer was stopped at ${d.toLocaleTimeString()}`);
+    }
+
 }
+//Write the HTML code so that you have one button that says Hush Ya'll. 
+//Write the JavaScript code to nag the user every 5 seconds with a message of Well... 
+//We're waiting!!! until you click the button. 
+//Clicking the button will stop the nage messages in the console.
+
